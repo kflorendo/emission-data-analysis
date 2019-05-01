c9ddc8f3d87c4326a1eac80c7f5977c14ce91a52
kflorendo <kflorendo1012@gmail.com>
2019-05-01 14:25:43 -0400

    Update laura-graph.py

--- laura-graph.py
+++ laura-graph.py
@@ -11,6 +11,8 @@ countries = []
 num_deaths = []
 pollution = []
 sizes = []
+font_sizes = []
+
 
 datareader = csv.reader(datafile) 
 headers = datareader.next() # read first row and store separately
@@ -21,8 +23,23 @@ for row in datareader:
         pollution.append(float(row[4]))
         sizes.append(float(row[5])/100)
 
+for i, size in enumerate(sizes):
+    if countries[i] in ["Mauritius", "Kenya", "Saint Lucia", "Antigua and Barbuda", "Israel", "Italy", "Uruguay", "Bahamas", "Ecuador", "Barbados", "Burundi", "Moldova", "Swaziland", "Haiti", "Algeria", "Mozambique", "Morocco", "Latvia", "Slovakia", "Botswana", "Sao Tome and Principe", "Dominican Republic", "Saint Vincent and the Grenadines", "Suriname", "Thailand", "Paraguay", "Colombia", "Iceland", "Ireland", "Panama", "Portugal", "Japan", "France", "Spain", "New Zealand", "Belgium"]:
+        font_sizes.append("no")
+    elif num_deaths[i] < 30:
+        font_sizes.append(5)
+    else:
+        if size > 300:
+            font_sizes.append(8)
+        elif size > 150:
+            font_sizes.append(6)
+        else:
+            font_sizes.append(5)
+
 plt.scatter(pollution, num_deaths, c=[[0,0,0.54,0.3]], s=sizes, edgecolors=[[0.769,0.847,0.902,0.3]])
-plt.annotate(countries, (pollution, num_deaths))
+for i, txt in enumerate(countries):
+    if font_sizes[i] != "no":
+        plt.annotate(txt, (pollution[i], num_deaths[i]), fontsize = font_sizes[i])
 plt.xlabel('Mean Annual PM2.5 Air Pollution Exposure (micrograms per cubic meter)')
 plt.ylabel('Number of Deaths (per 100,000)')
 plt.title('PM2.5 Air Pollution since 2010 vs. Death Rate')
